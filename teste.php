20b8e3adff71153927db2933411c4d7bf78e9a7d
Alexandre Bruno Cosmo <abcosmo04@.com>
2025-05-29 22:59:32 -0300

    teste gerando xls

--- teste.php
+++ teste.php
@@ -1,7 +1,28 @@
 <?php
-$senha = "Gu@rani69";
+header('Content-Type: application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename="dados.xls"');
 
-$salvar = password_hash($senha, PASSWORD_DEFAULT);
-echo $salvar;
-
+echo '<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Telefone</th>
+        <th>Data Cadastro</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>João Silva</td>
+        <td>joao@exemplo.com</td>
+        <td>(11) 9999-8888</td>
+        <td>2023-01-15</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Maria Souza</td>
+        <td>maria@exemplo.com</td>
+        <td>(21) 8888-7777</td>
+        <td>2023-02-20</td>
+    </tr>
+</table>';
 ?>
